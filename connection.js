cc3c56cf3964cf98936da4ffd5685778ecbb64fd
Ashitha31 <ashithajerry3112@gmail.com>
2023-03-14 09:29:02 +0530

    changes made

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "debanjan",
+    password: "123456",
     database: "keyvaultx"
 });
 
